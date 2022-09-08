8d1d0374ddc40b262c5f164656b60e32620d5ca4
Danny <danny@email.com>
2022-09-08 11:02:55 -0500

    delete

--- notes.py
+++
@@ -1,8 +0,0 @@
-go=input(print('Would you like to print out each individual record of the file? Y/N'))
-
-while go = 'Y':
-    print(f"student ID: [0]")
-    print(f"fname: [1]")
-    counter+=1
-    input=input(print('Next? Y/N'))
-    
